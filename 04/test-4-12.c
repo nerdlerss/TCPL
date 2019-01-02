382c89bca0d4b479d3b94bc22695a1fcf21005e9
nerdlerss <>
2019-01-02 19:19:16 +0800

    new some test file

---
+++ 04/test-4-12.c
@@ -0,0 +1,19 @@
+#include <math.h>
+int main(){
+    return 0;
+}
+
+void itoa(int s,char str[]){
+    //12313
+    int i;
+    if (s /10){
+        itoa(s/10,s);
+    }else{
+        if (s < 0){
+            str[i++] = '-';
+        }
+    }
+    str[i++]  = abs(s) + '0';
+    str[i] = '\0';
+
+}
---
+++ 04/test-4-13.c
@@ -0,0 +1,15 @@
+
+int main(){
+
+    return 0;
+}
+
+void reverse(char s[]){
+    void reverser(char s[], int i,int len);
+    reverser(s,0,strlen(s));
+}
+
+void reverser(char s[], int i,int len){
+    //abcd
+    
+}
