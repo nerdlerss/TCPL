b3c97415bd5a4e1b4e986ccd26eaedf3305e0f97
nerdlerss <nerdless@hotmail.com>
2019-08-01 19:58:33 +0800

    add test-2-6

--- 02/test-2-6.c
+++ 02/test-2-6.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+unsigned setbits(unsigned x,int p,int n,unsigned y);
 int main(){
-    int s ;
-    s = 7;
-    s = s << 3;
-    printf("%d",s);
+    unsigned int s ;
+    s = 77;
+    unsigned int b;
+    b = 22;
+    unsigned int sb = setbits(s,4,3,b);
+    printf("%d\n",sb);
     return 0;
 
 }
-unsigned set_bats(unsigned x,int p,int n,unsigned y){
-     
+unsigned setbits(unsigned x,int p,int n,unsigned y){
+    return x& ~(~(~0 << n) << (p+1-n)) |  (y&  ~( ~0 << n))  << (p+1-n); 
 }
 
  
