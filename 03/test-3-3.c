7fbdb7fd88bd70f117f0f1209a523368d177a374
nerdlerss <nerdlerss@hotmail.com>
2019-08-06 06:15:41 +0800

    编辑方法‘

--- 03/test-3-3.c
+++ 03/test-3-3.c
@@ -6,6 +6,20 @@ int main(){
 }
 
 void expand(char s1[],char s2[]){
-    
-
+	// a-z 
+	// -a-z 
+	// a-b-c 
+	// a-z0-9
+	// A-Z
+	// a - Z	
+		// 当状态是 x-x 这种才做操作
+	int i;
+	int i;
+	for(i =0; s1[i] != '\0';i++){
+		if((s1[i] <='z' && s1[i] >='a') || (s1[i] <='Z' && s1[i] >='A')){
+			//s[i] is word
+		}
+	}	
 }
+
+ 
