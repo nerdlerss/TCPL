b4326d2d31422fe107dedf938fa1f4fbe6ea0f4c
nerdlerss <nerdless@hotmail.com>
2019-08-06 20:13:42 +0800

    修改test3-3

--- 03/test-3-3.c
+++ 03/test-3-3.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+void expand(char s[],char v[]);
 int main(){
-
+    char s[]  = "-a-z--";
+    //-abcde--  
+    //-aaa-e--
+    //-a-e--
+    char v[1000];
+    expand(s,v);
+    printf("val -- > [%s]\n",v);
     return 0;
 }
 
@@ -14,10 +20,49 @@ void expand(char s1[],char s2[]){
 	// a - Z	
 		// 当状态是 x-x 这种才做操作
 	int i;
-	int i;
+	int state = 0 ;
+        int j = 0;
 	for(i =0; s1[i] != '\0';i++){
+		printf("state %d \n",state);
 		if((s1[i] <='z' && s1[i] >='a') || (s1[i] <='Z' && s1[i] >='A')){
 			//s[i] is word
+		     switch(state){
+			    case 0:
+				 state = 1;
+			         s2[j++] = s1[i];
+				 break;
+			    case 1:
+				 s2[j++] = s1[i];
+				 break;
+			    default:
+				//打印输出
+				if (s1[i] <='z' && s1[i] >='a' && (s1[i] <='z' && s1[i-2] >='a'&& s1[i]> s1[i-2]){
+				    int val = s[i-2];
+				    while(s[i] > val){
+					s[j++] = val;
+					val++;
+				    }
+				}else if (){
+				    continue;    
+				}else{
+				    s2[j++] = s1[i];
+				    state = 1;
+				    break; 
+				}
+			}
+
+		}else if(s1[i] == '-'){
+		    if(state == 0){
+			 s2[j++] = s1[i];	
+		    }else{
+			    //set state 
+			    s2[j++] = s1[i];
+			    state = 2;
+			}
+		       
+    		}else{
+		    state = 0;
+		    s2[j++] = s1[i];
 		}
 	}	
 }
