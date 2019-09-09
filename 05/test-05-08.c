72d3bcba0d0a3abf0b9007d0db3eb7815773ad8f
nerdlerss <nerdlerss@hotmail.com>
2019-09-09 22:48:27 +0800

    增加测试05-08

--- 05/test-05-08.c
+++ 05/test-05-08.c
@@ -1,43 +1,53 @@
+#include<stdio.h>
+
+int main(){ 
+   int day_of_year(int,int,int);
+   void mouth_day(int,int,int *p,int *d);
+   int today;
+   today = day_of_year(2019,3,23);
+   printf("to day : %d \n ",today);
+   
+   int pmouth,pday;
+   mouth_day(2015,322,&pmouth,&pday);
+   printf("pmouthday %d ,pday %d \n",pmouth,pday);
+}    
 static char daytab[2][13] = {
-    {0,31,28,31,30,31,30,31,31,30,31,30,31},
-    {0,31,29,31,30,31,30,31,31,30,31,30,31}
-};
+     {0,31,28,31,30,31,30,31,30,31,30,31},
+     {0,31,29,31,30,31,30,31,30,31,30,31},
+ };
 
-int  day_to_year(int year,int month,int day){
-    int i,leap ;
-    leap = year%4 == 0 && year%100 != 0 ||  year%400 == 0;
+int day_of_year(int year,int mouth,int day){
+    // 2018 23 1
     
-    if (month >12|| month <1){
+    int flag;
+    flag = year % 4 == 0 && year%100 == 0 || year %400 == 0;
+    if (mount <1 || mouth > 12){
+        return -1;  
+    }
+    if (day < 1 || day > day[flag][mouth]){
         return -1;
     }
-    if (day>1 || day>daytab[leap][month]){
-        return -1;
-    }
-    for (i=1;i<month;i++){
-        day += daytab[leap][i];
-    }
-    return day;
-}
 
-void month_day(int year,int yearday,int *pmonth, int *pday){
-    int i,leap; 
-    leap = (year%4 == 0) && (year%100 != 0) ||  (year%400 == 0);
-    if (year < 1){
-        *pmonth = -1;
-        *pday = -1;
-        return;
+    int i;
+
+    for(i =1; i < mouth ;i++){
+        day += daytab[flag][i];
+    }  
+    return day;
+} 
+void mouth_day(int year,int yestday, int *pmouth ,int *pday){
+    int flag;
+    flag = year %4 == 0&& year %100 == 0 || year %400 == 0;
+    if(year < 1){
+        return -1;
     }
-    for (i = 1;yearday > daytab[leap][i];i++){
-        yearday -= daytab[leap][i];
-    }
-    if(i>12 &&yearday > daytab[leap][12]){
-        *pmonth = -1;
-        *pday = -1;
-        return;
-    }
-    *pmonth = i;
-    *pday = yearday;
-}
-int main(){
-    return 0;
+
+    
+    int i;
+    for (i= 1; yestday > daytab[flag][i];i++)
+        yestday -= daytab[flag][i];
+
+    *pmouth = i;
+    *pday = yestday;
+
 }
